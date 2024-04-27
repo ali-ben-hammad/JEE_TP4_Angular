4558f3492a455f4a2bc2c651a5473a2a0c435769
ali-ben-hammad <ali.benhammad-etu@etu.univh2c.ma>
2024-04-27 20:46:50 +0100

    subscribe to the observable in the html file

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Product} from "../model/product.model";
-import {NgForOf} from "@angular/common";
+import {AsyncPipe, NgForOf} from "@angular/common";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {ProductService} from "../services/product.service";
@@ -9,6 +9,7 @@ import {ProductService} from "../services/product.service";
   standalone: true,
   imports: [
     NgForOf,
+    AsyncPipe,
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
@@ -17,15 +18,14 @@ export class ProductsComponent implements OnInit {
   constructor(private productsService: ProductService) {
   }
 
-  products: Array<Product> = [];
+  products$!: Observable<Array<Product>> ;
 
   ngOnInit(): void {
-    this.productsService.getProducts().subscribe({
-      next: (products : Array<Product> ) => {
-        this.products = products;
-      }
-    });
+    this.getProducts()
   }
+   getProducts() {
+    this.products$ = this.productsService.getProducts();
+   }
 
   onCheckChange($event: Event, product: Product) {
     this.productsService.checkProduct(product).subscribe({
