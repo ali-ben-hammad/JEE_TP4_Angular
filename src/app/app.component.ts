c070761591a42b2375b9e5575cc053e677fe3c58
ali-ben-hammad <ali.benhammad-etu@etu.univh2c.ma>
2024-04-27 15:22:43 +0100

    nav system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, NgForOf, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'HospitalFront';
+  routerOutlet = RouterOutlet;
+  actions : Array<any> = [
+    { name: 'Home', path: 'home' , icon: 'bi-house' },
+    { name: 'Products', path: 'products' , icon: 'bi-cart' },
+    { name: 'Product', path: 'product' , icon: 'bi-plus' },
+  ];
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProductsComponent} from "./products/products.component";
+import {ProductComponent} from "./product/product.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path : "home" , component : HomeComponent },
+  { path : "" , redirectTo : "home" , pathMatch : "full" },
+  { path : "products" , component : ProductsComponent },
+  { path : "product" ,  component : ProductComponent },
+];
